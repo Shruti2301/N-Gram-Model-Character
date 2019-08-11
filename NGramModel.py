73b3b57a7dbd6071ae2a8e0cae080baa2cd0e081
Shruti Mandaokar <shruti.mandaokar@gmail.com>
2019-08-11 19:47:47 +0530

    Add files via upload

---
+++ NGramModel.py
@@ -0,0 +1,30 @@
+# N Gram Modelling
+
+import random
+
+text = """Global warming or climate change has become a worldwide concern. It is gradually developing into an unprecedented environmental crisis evident in melting glaciers, changing weather patterns, rising sea levels, floods, cyclones and droughts. Global warming implies an increase in the average temperature of the Earth due to entrapment of greenhouse gases in the earth’s atmosphere."""
+
+n = 5
+
+ngrams = {}
+
+#Create the Ngrams
+for i in range(len(text)-n):
+    gram = text[i:i+n] #text[0:3] =Glo
+    if gram not in ngrams.keys():
+        ngrams[gram] = []
+    ngrams[gram].append(text[i+n]) #text[0+3] = text[3] = b
+    
+#Testing the Ngram Model
+currentGram = text[0:n]
+result = currentGram
+for i in range(100):
+    if currentGram not in ngrams.keys():
+        break
+    
+    possibilities = ngrams[currentGram]
+    nextItem = possibilities[random.randrange(len(possibilities))]
+    result += nextItem
+    currentGram = result[len(result)-n:len(result)]
+ 
+print(result)    
